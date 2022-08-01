3c4d6ad5a1563e80036474b7ea17e6e240bae7ee
Think Link <software@thinklink.com.co>
2022-08-01 17:52:55 -0500

    Delete body-parser

--- routes/smartgreen_routes.js
+++ routes/smartgreen_routes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const mysqlConnection = require('../db/database.js');
-const bodyParser = require('body-parser');
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
 
 async function getSmartGreenHouseData () {
 
